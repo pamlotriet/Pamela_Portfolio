95ca9ff8a3ba1aa326e55950788aa042170af45e
Pamela Lotriet <plotriet@retrorabbit.co.za>
2024-06-06 12:18:06 +0200

    Addedmhome page, translation and base components for other pages as well as button component

--- pamela-portfolio/tailwind.config.js
+++ pamela-portfolio/tailwind.config.js
@@ -4,6 +4,7 @@ module.exports = {
   theme: {
     colors: {
       primary: "#0F0E3E",
+      white:"#FFFFFF"
     },
     extend: {
       width: {
@@ -13,6 +14,17 @@ module.exports = {
       },
       height: {
         500: "500px",
+      },
+      borderRadius: {
+        '20px': '20px',
+      },
+      boxShadow: {
+        'custom': '-6px -6px 7px #4644bf, 8px 8px 12px rgba(0, 0, 0, 0.7)',
+        'custom-hover': '-2px -2px 4px #4644bf, 2px 2px 4px rgba(0, 0, 0, 0.7)',
+        'custom-focus': 'inset 2px 4px 12px rgba(0, 0, 0, 0.7), inset -2px -4px 8px #4644bf',
+      },
+      fontFamily: {
+        bangers: ['Bangers', 'sans-serif'],
       },
     },
   },
