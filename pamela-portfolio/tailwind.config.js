1a70ea16c1c6bd5558215ad25a58633996544545
Pamela Lotriet <plotriet@retrorabbit.co.za>
2024-06-06 14:09:50 +0200

    updated about page

--- pamela-portfolio/tailwind.config.js
+++ pamela-portfolio/tailwind.config.js
@@ -10,10 +10,14 @@ module.exports = {
       width: {
         635: "635px",
         40: "40%",
-        50: "50%"
+        50: "50%",
+        340: '340px',
+        285: '285px',
       },
       height: {
         500: "500px",
+        591: '591px',
+        536: '536px',
       },
       borderRadius: {
         '20px': '20px',
@@ -22,6 +26,7 @@ module.exports = {
         'custom': '-6px -6px 7px #4644bf, 8px 8px 12px rgba(0, 0, 0, 0.7)',
         'custom-hover': '-2px -2px 4px #4644bf, 2px 2px 4px rgba(0, 0, 0, 0.7)',
         'custom-focus': 'inset 2px 4px 12px rgba(0, 0, 0, 0.7), inset -2px -4px 8px #4644bf',
+        'custom-image-container': 'inset 6px 6px 15px rgba(0, 0, 0, 0.8), inset -4px -4px 15px #4644bf'
       },
       fontFamily: {
         bangers: ['Bangers', 'sans-serif'],
